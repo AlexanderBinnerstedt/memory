a28c2763d310e223a6bf31f0d9af5cabd9a5eff1
AlexanderBinnerstedt <alexander@binnerstedt.com>
2021-03-05 00:11:25 +0100

    add flip and shuffle

--- main.js
+++ main.js
@@ -1,27 +1,48 @@
 "use strict";
 
-var cardsclicked = 0;
 
 const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
+let cardsclicked = false;
+var value1;
+var value2;
+let firstCard, secondCard;
+shuffle();
+
 function flipCard() {
 
     if (!this.classList.contains('flip')) {
 
-        if (cardsclicked < 2) {
+        if (cardsclicked == false) {
             this.classList.toggle('flip');
-            cardsclicked++;
-            
+            value1 = document.getElementById(this.id).getAttribute("value");
+            cardsclicked = true;
+            firstCard = this;
+        } else if (cardsclicked == true) {
+            this.classList.toggle('flip');
+            value2 = document.getElementById(this.id).getAttribute("value");
+            cardsclicked = false;
+            secondCard = this;
+
+            if (value1 == value2){ //Om korten är samma
+                console.log("they are the same");
+                firstCard.removeEventListener('click',flipCard)
+                secondCard.removeEventListener('click',flipCard)
+            } else{ //Om korten inte är samma
+                setTimeout(() => {
+                    firstCard.classList.toggle('flip')
+                    secondCard.classList.toggle('flip')
+                }, 1500);
+            }
         }
     }
-
-    if (cardclicked == 2) {
-        
-    }
-
 }
 
-
-
+function shuffle() {
+    cards.forEach(card => {
+      let randomPos = Math.floor(Math.random() * 12);
+      card.style.order = randomPos;
+    });
+}
