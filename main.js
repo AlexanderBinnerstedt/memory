ca5261a256829f25a8f75fc9ffccfd2daef021ff
AlexanderBinnerstedt <alexander@binnerstedt.com>
2021-03-08 22:39:13 +0100

    styling

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => card.addEventListener('click', flipCard));
@@ -9,40 +8,53 @@ let cardsclicked = false;
 var value1;
 var value2;
 let firstCard, secondCard;
+let lock = false;
+var fails = 0;
+document.getElementById('fails').innerHTML = "incorrect choices: " + String(fails);
 shuffle();
 
 function flipCard() {
-
+    if (lock) return;
+        
     if (!this.classList.contains('flip')) {
 
-        if (cardsclicked == false) {
+        if (cardsclicked == false) {            //Första kortet
             this.classList.toggle('flip');
             value1 = document.getElementById(this.id).getAttribute("value");
             cardsclicked = true;
             firstCard = this;
-        } else if (cardsclicked == true) {
+        } else if (cardsclicked == true) {     //Andra kortet
             this.classList.toggle('flip');
             value2 = document.getElementById(this.id).getAttribute("value");
             cardsclicked = false;
             secondCard = this;
 
-            if (value1 == value2){ //Om korten är samma
-                console.log("they are the same");
-                firstCard.removeEventListener('click',flipCard)
-                secondCard.removeEventListener('click',flipCard)
-            } else{ //Om korten inte är samma
-                setTimeout(() => {
-                    firstCard.classList.toggle('flip')
-                    secondCard.classList.toggle('flip')
-                }, 1500);
-            }
+            checkMatch();
         }
+    }
+}
+
+function checkMatch() {
+
+    if (value1 == value2){ //Om korten är samma
+        firstCard.removeEventListener('click',flipCard)
+        secondCard.removeEventListener('click',flipCard)
+    } else {            //Om korten inte är samma
+        lock = true;
+        fails++;
+        console.log(document.getElementById('fails'));
+        document.getElementById('fails').innerHTML = "incorrect choices: " + String(fails);
+        setTimeout(() => {
+            firstCard.classList.toggle('flip')
+            secondCard.classList.toggle('flip')
+            lock = false;
+        }, 1500);
     }
 }
 
 function shuffle() {
     cards.forEach(card => {
-      let randomPos = Math.floor(Math.random() * 12);
+      let randomPos = Math.floor(Math.random() * 16);
       card.style.order = randomPos;
     });
 }
